ff19e868ece5ffc9205e9021c137a87502d296d9
dd0129 <tanghongdi@hotmail.com>
2014-03-31 23:24:00 +0800

    update code

---
+++ halley-core/src/main/java/com/dianping/data/warehouse/executer/StartScheduler.java
@@ -0,0 +1,14 @@
+package com.dianping.data.warehouse.executer;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by adima on 14-3-31.
+ */
+public class StartScheduler {
+    public static void main(String[] args){
+        new ClassPathXmlApplicationContext("classpath:spring-applicationcontext.xml");
+
+    }
+}
